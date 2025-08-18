ff11d798f90febc415125fc3138edb6f5a9b61b9
annakalinkin <annakalinkinakv57@gmail.com>
2025-08-18 16:40:37 +0300

    диплом

--- test_UI.py
+++ test_UI.py
@@ -6,12 +6,12 @@ from pages.main_page import MainPage
 @allure.feature("Поиск книжной информации")
 class TestBookSearch:
     @allure.title("Поиск книги по заголовку")
-    @allure.description("Тест проверяет возможность поиска книги по заголовку 'капитанская'.")
+    @allure.description("Тест проверяет возможность поиска книги по заголовку 'тайна лисьей норы'.")
     def test_by_name(self, driver):
         main_page = MainPage(driver)
         main_page.open()
-        main_page.search_book("капитанская")
-        assert "Показываем результаты по запросу «капитанская», найдено:" in main_page.get_search_results_text()
+        main_page.search_book("тайна лисьей норы")
+        assert "Показываем результаты по запросу «тайна лисьей норы», найдено:" in main_page.get_search_results_text()
 
 
     @allure.title("Поиск книги с символами вместо названия")
@@ -19,5 +19,22 @@ class TestBookSearch:
     def test_negative_by_symbols(self, driver):
         main_page = MainPage(driver)
         main_page.open()
-        main_page.search_book("#$%")
-        assert "Поиск по запросу «#$%» не принёс результатов" in main_page.get_search_results_text()
+        main_page.search_book("%&*#")
+        assert "Поиск по запросу «%&*#» не принёс результатов" in main_page.get_search_results_text()
+
+
+        @allure.title("Поиск книги  с символами и нозванием ")
+        @allure.description("Тест проверяет поиск книги с символами и нозванием")
+        def test_negative_by_symbols(self, driver):
+            main_page = MainPage(driver)
+            main_page.open()
+            main_page.search_book("$$$тайна лисьей норы")
+            assert "показывает результаты по запросу  «$$$тайна лисьей норы» , найдено" in main_page.get_search_results_text()
+
+            @allure.title("Поиск книги с неполным названием ")
+            @allure.description("Тест проверяет поиск книги с неполным нозванием")
+            def test_negative_by_symbols(self, driver):
+                main_page = MainPage(driver)
+                main_page.open()
+                main_page.search_book(" нора")
+                assert "Поиск по запросу « нора» не принёс результатов" in main_page.get_search_results_text()
--- test_api.py
+++ test_api.py
@@ -11,13 +11,32 @@ base_url = "https://web-gate.chitai-gorod.ru/api/v2/"
 @allure.title("Тестирование поиска книги по автору")
 @allure.description("Проверка, что API возвращает книги с ожидаемым автором.")
 def test_api_book_by_author():
-    resp = requests.get(f"{base_url}search/product?phrase=джоан роулинг", headers=headers)
+    resp = requests.get(f"{base_url}search/product?phrase=виктория дьякова", headers=headers)
     assert resp.status_code == 200
+
 
 @allure.epic("API Тестирование")
 @allure.feature("Поиск книг")
-@allure.title("Тестирование поиска с недопустимой японской фразой")
-@allure.description("Проверка, что API возвращает ошибку при поиске с недопустимой японской фразой.")
+@allure.title("Тестирование поиска с недопустимой китайской фразой")
+@allure.description("Проверка, что API возвращает ошибку при поиске с недопустимой китайской фразой.")
 def test_negative_api_Japanese():
-    resp = requests.get(f"{base_url}search/product?phrase=人で座ってください", headers=headers)
+    resp = requests.get(f"{base_url}search/product?phrase=你好", headers=headers)
     assert resp.status_code == 422
+
+
+@allure.epic("API Тестирование")
+@allure.feature("Получение категорий книг")
+@allure.title("Тестирование получения списка категорий книг")
+@allure.description("Проверка, что API возвращает успешный ответ с доступными категориями книг.")
+def test_api_get_categories():
+    resp = requests.get(f"{base_url}categories", headers=headers)
+    assert resp.status_code == 200
+
+
+    @allure.epic("API Тестирование")
+    @allure.feature("Поиск книг")
+    @allure.title("Тестирование поиска с пустым запросом")
+    @allure.description("Проверка, что API возвращает ошибку при поиске с пустым запросом.")
+    def test_negative_api_empty_query():
+        resp = requests.get(f"{base_url}search/product?phrase=", headers=headers)
+        assert resp.status_code == 400
